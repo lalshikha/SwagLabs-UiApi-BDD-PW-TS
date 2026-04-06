8b782129ee563e66ea61ccdf0ee0e08778b08e5c
SLG <slgwfhpersonal@email.com>
2026-04-06 16:27:27 +0530

    Revert "Merge branch 'main' of https://github.com/lalshikha/SwagLabs-UiApi-BDD-PW-TS"
    This reverts commit 349ca1ca0a1ef750ad9b8dc4c6ac3880d2553726, reversing
    changes made to 06889abe6db932cef775b66a54a4b70547ccb5ad.

--- src/pages/InventoryPage.ts
+++
@@ -1,5 +0,0 @@
-import { Locator, expect } from '@playwright/test';
-import BasePage from './BasePage';
-import { L, type LocatorKey } from '../config/config_locators';
-
-export default class InventoryPage extends BasePage {}
--- src/step-definitions/ui/inventorySteps.ts
+++
@@ -1,4 +0,0 @@
-import { When, Then } from '../../fixtures/Fixtures';
-import { L } from '../../config/config_locators';
-
-
